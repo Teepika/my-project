2aab13733bebc445aec9d8e031129d26acf6903c
Teepika <teepika057@gmail.com>
2023-10-14 19:54:38 +0530

    Implemented python file for find max number

---
+++ Max.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+numbers = [12, 45, 78, 32, 98, 54, 23]
+
+# Find the maximum number in the list
+maximum = max(numbers)
+
+print("The maximum number is:", maximum)
+
+
+# In[ ]:
+
+
+
+
