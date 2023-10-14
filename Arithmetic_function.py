99a29f703deb52e9d90f2fc6b4d4e71e55295f6c
Teepika <teepika057@gmail.com>
2023-10-14 19:40:38 +0530

    Added first python file

---
+++ Arithmetic_function.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+# Addition
+result_add = 5 + 3
+print("5 + 3 =", result_add)
+
+# Subtraction
+result_subtract = 10 - 4
+print("10 - 4 =", result_subtract)
+
+# Multiplication
+result_multiply = 6 * 2
+print("6 * 2 =", result_multiply)
+
+# Division
+result_divide = 8 / 4
+print("8 / 4 =", result_divide)
+
+# Exponentiation
+result_exponent = 2 ** 3
+print("2 ** 3 =", result_exponent)
+
+
+# In[ ]:
+
+
+
+
